1f181b635625312782d8e7911f5df21b75714d06
Bogdanov Anton <kortirso@gmail.com>
2025-07-10 16:23:57 +0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+import { CharKeeperApp } from './CharKeeperApp/CharKeeperApp';
+
+render(() => <CharKeeperApp />, document.getElementById('root'));
