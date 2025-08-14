b61f41655200c9f6ea2aa62fcd3afee15be530b8
Bogdanov Anton <kortirso@gmail.com>
2025-08-14 20:46:41 +0300

    git subrepo pull src/CharKeeperApp
    subrepo:
      subdir:   "src/CharKeeperApp"
      merged:   "b7d7bd6"
    upstream:
      origin:   "git@github.com:kortirso/charkeeper_solid_app.git"
      branch:   "main"
      commit:   "b7d7bd6"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "4f60dd7"

--- src/CharKeeperApp/pages/Content/Homebrew/Subclasses.jsx
+++ src/CharKeeperApp/pages/Content/Homebrew/Subclasses.jsx
@@ -2,17 +2,20 @@ import { createSignal, Switch, Match, batch, Show, For } from 'solid-js';
 import * as i18n from '@solid-primitives/i18n';
 
 import { NewDaggerheartSubclassForm, DaggerheartSubclass } from '../../../pages';
-import { ContentWrapper, Button, IconButton, Toggle } from '../../../components';
+import { ContentWrapper, Button, IconButton, Toggle, Input } from '../../../components';
 import { Close } from '../../../assets';
 import { useAppState, useAppLocale, useAppAlert } from '../../../context';
 import { createHomebrewSubclassRequest } from '../../../requests/createHomebrewSubclassRequest';
 import { removeHomebrewSubclassRequest } from '../../../requests/removeHomebrewSubclassRequest';
+import { copyHomebrewSubclassRequest } from '../../../requests/copyHomebrewSubclassRequest';
+import { copyToClipboard } from '../../../helpers';
 
 export const HomebrewSubclasses = (props) => {
   const [activeView, setActiveView] = createSignal('left');
+  const [copySubclassId, setCopySubclassId] = createSignal('');
 
   const [appState] = useAppState();
-  const [{ renderAlerts }] = useAppAlert();
+  const [{ renderAlerts, renderNotice, renderAlert }] = useAppAlert();
   const [, dict] = useAppLocale();
 
   const t = i18n.translator(dict);
@@ -30,12 +33,24 @@ export const HomebrewSubclasses = (props) => {
     } else renderAlerts(result.errors);
   }
 
+  const copySubclass = async () => {
+    const result = await copyHomebrewSubclassRequest(appState.accessToken, props.provider, copySubclassId());
+
+    if (result.errors === undefined) props.reloadHomebrews();
+    else renderAlert(result.errors);
+  }
+
   const removeSubclass = async (event, id) => {
     event.stopPropagation();
 
     const result = await removeHomebrewSubclassRequest(appState.accessToken, props.provider, id);
     if (result.errors === undefined) props.removeHomebrew('subclasses', id);
     else renderAlerts(result.errors);
+  }
+
+  const copy = (value) => {
+    copyToClipboard(value);
+    renderNotice(t('alerts.copied'));
   }
 
   return (
@@ -47,6 +62,17 @@ export const HomebrewSubclasses = (props) => {
             <Button default classList="mb-2" onClick={() => setActiveView('right')}>
               {t(`pages.homebrewPage.${props.provider}.newSubclass`)}
             </Button>
+            <div class="flex mb-2">
+              <Button default size="small" classList="px-2" onClick={copySubclass}>
+                {t('copy')}
+              </Button>
+              <Input
+                containerClassList="ml-2 flex-1"
+                placeholder={t(`pages.homebrewPage.${props.provider}.copyRacePlaceholder`)}
+                value={copySubclassId()}
+                onInput={(value) => setCopySubclassId(value)}
+              />
+            </div>
             <Show when={props.homebrews !== undefined}>
               <For each={props.homebrews.subclasses}>
                 {(item) =>
@@ -63,6 +89,9 @@ export const HomebrewSubclasses = (props) => {
                         <DaggerheartSubclass homebrews={props.homebrews} item={item} />
                       </Match>
                     </Switch>
+                    <p class="absolute bottom-0 right-0 px-2 py-1 dark:text-snow cursor-pointer" onClick={() => copy(item.id)}>
+                      COPY
+                    </p>
                   </Toggle>
                 }
               </For>
