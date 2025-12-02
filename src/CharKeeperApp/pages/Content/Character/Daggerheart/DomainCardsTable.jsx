6d7751431a4d5055f9bdbcd854bff5465f2df54a
Bogdanov Anton <kortirso@gmail.com>
2025-12-02 15:28:33 +0300

    git subrepo pull src/CharKeeperApp
    subrepo:
      subdir:   "src/CharKeeperApp"
      merged:   "fa70eb7"
    upstream:
      origin:   "git@github.com:kortirso/charkeeper_solid_app.git"
      branch:   "main"
      commit:   "fa70eb7"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "4f60dd7"

--- src/CharKeeperApp/pages/Content/Character/Daggerheart/DomainCardsTable.jsx
+++ src/CharKeeperApp/pages/Content/Character/Daggerheart/DomainCardsTable.jsx
@@ -1,60 +1,108 @@
-import { For, Show, Switch, Match } from 'solid-js';
+import { createMemo, For, Show, Switch, Match } from 'solid-js';
 
 import { Button } from '../../../../components';
+import config from '../../../../data/daggerheart.json';
+import { useAppLocale } from '../../../../context';
 import { Close, Arrow } from '../../../../assets';
+
+const TRANSLATION = {
+  en: {
+    spell: 'Spell',
+    ability: 'Ability',
+    grimoire: 'Grimoire',
+    level: 'Level'
+  },
+  ru: {
+    spell: 'Заклинание',
+    ability: 'Способность',
+    grimoire: 'Гримуар',
+    level: 'Уровень'
+  }
+}
 
 export const DomainCardsTable = (props) => {
   const spells = () => props.spells;
 
+  const [locale] = useAppLocale();
+
+  const cardsByDomains = createMemo(() => {
+    if (props.countCards === undefined) return undefined;
+
+    const domains = spells().reduce((acc, item) => {
+      acc[item.origin_value] ? acc[item.origin_value] += 1 : acc[item.origin_value] = 1
+      return acc;
+    }, {});
+    console.log(Object.entries(domains).sort((a, b) => b[1] - a[1]));
+    return Object.entries(domains).sort((a, b) => b[1] - a[1]);
+  });
+
   return (
-    <div class="blockable p-4 mb-2">
-      <h2 class="text-lg dark:text-snow">{props.title}</h2>
-      <Show when={props.subtitle}>
-        <p class="text-sm dark:text-snow">{props.subtitle}</p>
-      </Show>
-      <Show when={spells().length > 0}>
-        <table class="w-full table table-top first-column-full-width">
-          <thead>
-            <tr>
-              <td />
-              <td />
-            </tr>
-          </thead>
-          <tbody>
-            <For each={spells()}>
-              {(spell) =>
-                <tr>
-                  <td class="py-1 pl-1">
-                    <p class="cursor-pointer dark:text-snow" onClick={() => props.onChangeSpell(spell)}>{spell.title}</p>
-                    <p class="text-xs dark:text-snow">{spell.description}</p>
-                    <Show when={spell.notes}>
-                      <p class="text-sm mt-1 dark:text-snow">{spell.notes}</p>
-                    </Show>
-                  </td>
-                  <td class="py-1">
-                    <div class="flex flex-col flex-col-reverse md:flex-row items-center gap-y-4 gap-x-2">
-                      <Switch>
-                        <Match when={spell.ready_to_use}>
-                          <Button default size="small" onClick={() => props.onUpdateCharacterSpell(spell, { character_spell: { ready_to_use: false } })}>
-                            <Arrow bottom width={16} height={16} />
-                          </Button>
-                        </Match>
-                        <Match when={!spell.ready_to_use}>
-                          <Button default size="small" onClick={() => props.onUpdateCharacterSpell(spell, { character_spell: { ready_to_use: true } })}>
-                            <Arrow top width={16} height={16} />
-                          </Button>
-                        </Match>
-                      </Switch>
-                      <Button default size="small" onClick={() => props.onRemoveCharacterSpell(spell)}>
-                        <Close />
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
+    <div class="blockable p-4 mb-2 dark:text-snow">
+      <div class="flex justify-between">
+        <div>
+          <h2 class="text-lg">{props.title}</h2>
+          <Show when={props.subtitle}>
+            <p class="text-sm">{props.subtitle}</p>
+          </Show>
+        </div>
+        <Show when={cardsByDomains() !== undefined}>
+          <div>
+            <For each={cardsByDomains()}>
+              {(item) =>
+                <p class="text-sm">{config.domains[item[0]].name[locale()]} - {item[1]}</p>
               }
             </For>
-          </tbody>
-        </table>
+          </div>
+        </Show>
+      </div>
+      <Show when={spells().length > 0}>
+        <div class="mt-4">
+          <For each={spells()}>
+            {(spell) =>
+              <div class="even:bg-stone-100 dark:even:bg-dark-dusty p-1">
+                <div class="cursor-pointer mb-2" onClick={() => props.onChangeSpell(spell)}>
+                  <p class="font-normal! text-lg">{spell.title}</p>
+                  <Show when={spell.info.type}>
+                    <p class="text-sm">
+                      {config.domains[spell.origin_value].name[locale()]} ({spell.level} {TRANSLATION[locale()].level}), {TRANSLATION[locale()][spell.info.type]}
+                    </p>
+                  </Show>
+                </div>
+                <p
+                  class="feat-markdown text-xs mb-1"
+                  innerHTML={spell.description} // eslint-disable-line solid/no-innerhtml
+                />
+                <Show when={spell.notes}>
+                  <p class="text-sm mb-1">{spell.notes}</p>
+                </Show>
+                <div class="flex flex-row items-center justify-end gap-y-4 gap-x-2">
+                  <Switch>
+                    <Match when={spell.ready_to_use}>
+                      <Button default size="small" onClick={() => props.onUpdateCharacterSpell(spell, { character_spell: { ready_to_use: false } })}>
+                        <Arrow bottom width={16} height={16} />
+                      </Button>
+                    </Match>
+                    <Match when={!spell.ready_to_use}>
+                      <Button default size="small" classList="px-1" onClick={() => props.onUpdateCharacterSpell(spell, { character_spell: { ready_to_use: true } })}>
+                        <div class="flex items-center">
+                          <Show when={spell.info.recall !== undefined}>
+                            <p class="px-1 text-center font-normal! text-lg">
+                              {spell.info.recall}
+                            </p>
+                          </Show>
+                          <Arrow top width={16} height={16} />
+                        </div>
+                      </Button>
+                    </Match>
+                  </Switch>
+                  <Button default size="small" classList="py-0.5" onClick={() => props.onRemoveCharacterSpell(spell)}>
+                    <Close />
+                  </Button>
+                </div>
+              </div>
+            }
+          </For>
+        </div>
       </Show>
     </div>
   );
